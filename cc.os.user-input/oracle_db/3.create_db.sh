2f21ecb9e065c4f1435646b1487e606d512ca9db
Mehdi Ahmadi <aphorise@gmail.com>
2021-08-04 13:42:10 +0200

    Added missing escape chracter in SQL restore statement.

--- cc.os.user-input/oracle_db/3.create_db.sh
+++ cc.os.user-input/oracle_db/3.create_db.sh
@@ -13,7 +13,7 @@ declare
 restoreexist integer;
 
 begin
-  select count(*) into restoreexist from V$RESTORE_POINT where NAME='GOOD';
+  select count(*) into restoreexist from V\$RESTORE_POINT where NAME='GOOD';
   if (restoreexist = 0) then
     execute immediate 'create restore point good';
   end if;
